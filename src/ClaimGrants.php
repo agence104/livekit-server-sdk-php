aee30e8e486f420b5c5e057ecc03644ac455e5da
Patrick Delorme <pdelorme@agence104.com>
2022-02-11 10:08:58 -0500

    Initial Package

---
+++ src/ClaimGrants.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Agence104\LiveKit;
+
+class ClaimGrants {
+  /**
+   * The Access Token Grants
+   *
+   * @var string
+   */
+  public $name;
+
+  /**
+   * The Access Token Grants
+   *
+   * @var \Agence104\LiveKit\VideoGrant
+   */
+  public $video;
+  /**
+   * The Access Token Grants
+   *
+   * @var string
+   */
+  public $metadata;
+  /**
+   * The Access Token Grants
+   *
+   * @var string
+   */
+  public $sha256;
+}
